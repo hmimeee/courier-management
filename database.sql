cb58712924a63676aa95259e881953fc5ccf4d36
hmimeee <hmimeee@gmail.com>
2020-09-18 12:00:11 +0600

    Added preview image and database file

--- database.sql
+++ database.sql
@@ -1,125 +1,250 @@
--- --------------------------------------------------------
--- Host:                         localhost
--- Server version:               5.7.24 - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Version:             10.2.0.5599
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.9.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Sep 18, 2020 at 05:58 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Dumping structure for table courier.invoice
+--
+-- Database: `courier`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice`
+--
+
 DROP TABLE IF EXISTS `invoice`;
-CREATE TABLE IF NOT EXISTS `invoice` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `invoice` (
+  `id` int(10) UNSIGNED NOT NULL,
   `order_id` int(10) NOT NULL DEFAULT '0',
-  `user_id` int(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `order_id` (`order_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `user_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table courier.invoice: ~1 rows (approximately)
-DELETE FROM `invoice`;
-/*!40000 ALTER TABLE `invoice` DISABLE KEYS */;
+--
+-- Truncate table before insert `invoice`
+--
+
+TRUNCATE TABLE `invoice`;
+--
+-- Dumping data for table `invoice`
+--
+
 INSERT INTO `invoice` (`id`, `order_id`, `user_id`) VALUES
-	(15, 3, 38),
-	(16, 4, 39);
-/*!40000 ALTER TABLE `invoice` ENABLE KEYS */;
+(15, 3, 38),
+(16, 4, 39);
 
--- Dumping structure for table courier.orders
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
 DROP TABLE IF EXISTS `orders`;
-CREATE TABLE IF NOT EXISTS `orders` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `orders` (
+  `id` int(11) UNSIGNED NOT NULL,
   `item_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
-  `store_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `store_id` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `recipient` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `recipient_number` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `recipient_address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT '',
   `amount` int(11) DEFAULT NULL,
   `fee` int(11) DEFAULT NULL,
   `payment_status` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT '',
-  `status_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `status_id` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `delivery_time` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT '',
-  `user_id` int(11) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `user_id` int(11) UNSIGNED NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table courier.orders: ~3 rows (approximately)
-DELETE FROM `orders`;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+--
+-- Truncate table before insert `orders`
+--
+
+TRUNCATE TABLE `orders`;
+--
+-- Dumping data for table `orders`
+--
+
 INSERT INTO `orders` (`id`, `item_name`, `store_id`, `recipient`, `recipient_number`, `recipient_address`, `amount`, `fee`, `payment_status`, `status_id`, `delivery_time`, `created_at`, `user_id`) VALUES
-	(3, 'Imran', 15, 'HMI', '01782448244', 'Dhaka', 500, 20, 'Unpaid', 1, '12 Hours', '26-03-2020', 38),
-	(4, 'Order Name', 17, 'HMI', '01782448244', 'Dhaka', 500, 20, 'Paid', 1, '12 Hours', '27-03-2020', 39),
-	(5, 'New Order', 17, 'HMI Me', '01782448244', 'Dhaka', 500, 20, 'Paid', 5, '12 Hours', '27-03-2020', 39);
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+(3, 'Imran', 15, 'HMI', '01782448244', 'Dhaka', 500, 20, 'Unpaid', 1, '12 Hours', '26-03-2020', 38),
+(4, 'Order Name', 17, 'HMI', '01782448244', 'Dhaka', 500, 20, 'Paid', 1, '12 Hours', '27-03-2020', 39),
+(5, 'New Order', 17, 'HMI Me', '01782448244', 'Dhaka', 500, 20, 'Paid', 5, '12 Hours', '27-03-2020', 39);
 
--- Dumping structure for table courier.status
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
 DROP TABLE IF EXISTS `status`;
-CREATE TABLE IF NOT EXISTS `status` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `status` (
+  `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table courier.status: ~5 rows (approximately)
-DELETE FROM `status`;
-/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+--
+-- Truncate table before insert `status`
+--
+
+TRUNCATE TABLE `status`;
+--
+-- Dumping data for table `status`
+--
+
 INSERT INTO `status` (`id`, `name`, `description`) VALUES
-	(1, 'Pending', 'Pending for Confirmation'),
-	(2, 'Ready', 'Ready to Deliver'),
-	(3, 'Delivered', 'Delivery Confirmed'),
-	(4, 'Returned', 'Product Returned'),
-	(5, 'Cancelled', 'Delivery Cancelled');
-/*!40000 ALTER TABLE `status` ENABLE KEYS */;
+(1, 'Pending', 'Pending for Confirmation'),
+(2, 'Ready', 'Ready to Deliver'),
+(3, 'Delivered', 'Delivery Confirmed'),
+(4, 'Returned', 'Product Returned'),
+(5, 'Cancelled', 'Delivery Cancelled');
 
--- Dumping structure for table courier.stores
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stores`
+--
+
 DROP TABLE IF EXISTS `stores`;
-CREATE TABLE IF NOT EXISTS `stores` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `stores` (
+  `id` int(11) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `user_id` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `user_id` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table courier.stores: ~2 rows (approximately)
-DELETE FROM `stores`;
-/*!40000 ALTER TABLE `stores` DISABLE KEYS */;
+--
+-- Truncate table before insert `stores`
+--
+
+TRUNCATE TABLE `stores`;
+--
+-- Dumping data for table `stores`
+--
+
 INSERT INTO `stores` (`id`, `name`, `address`, `user_id`) VALUES
-	(15, 'Store', '', NULL),
-	(16, 'HMI Store', 'Dhaka', 38),
-	(17, 'Raul Lacy', 'Dhaka', 39);
-/*!40000 ALTER TABLE `stores` ENABLE KEYS */;
+(15, 'Store', '', NULL),
+(16, 'HMI Store', 'Dhaka', 38),
+(17, 'Raul Lacy', 'Dhaka', 39);
 
--- Dumping structure for table courier.users
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `phone` (`phone`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table courier.users: ~2 rows (approximately)
-DELETE FROM `users`;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+--
+-- Truncate table before insert `users`
+--
+
+TRUNCATE TABLE `users`;
+--
+-- Dumping data for table `users`
+--
+
 INSERT INTO `users` (`id`, `name`, `email`, `phone`, `address`, `password`, `role`) VALUES
-	(38, 'Admin', 'admin@example.com', '09887885657', 'Dhaka', 'e10adc3949ba59abbe56e057f20f883e', 'admin'),
-	(39, 'Imran', 'bfihelp@gmail.com', '', '', 'e10adc3949ba59abbe56e057f20f883e', 'marchant');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+(38, 'Admin', 'admin@hmime.com', '09887885657', 'Dhaka', 'e10adc3949ba59abbe56e057f20f883e', 'admin'),
+(39, 'Imran', 'me@hmime.com', '', '', 'e10adc3949ba59abbe56e057f20f883e', 'marchant');
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stores`
+--
+ALTER TABLE `stores`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `phone` (`phone`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `stores`
+--
+ALTER TABLE `stores`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
